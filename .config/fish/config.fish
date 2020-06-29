832503ae503ee05816fad13e49aa67ac48f0b914
Matan Kushner <hello@matchai.dev>
2020-06-29 18:47:48 -0400

    fish: add direnv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,13 @@
 source ~/.config/fish/alias.fish
 
-# Configure Jump
-status --is-interactive; and source (jump shell fish | psub)
-
 # Load all saved ssh keys
 /usr/bin/ssh-add -A ^/dev/null
 
-# Install Starship
+# Setup Jump
+status --is-interactive; and source (jump shell fish | psub)
+
+# Setup Direnv
+eval (direnv hook fish)
+
+# Setup Starship
 starship init fish | source
