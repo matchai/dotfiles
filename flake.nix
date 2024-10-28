db45bc5cb97cd69013432274f45b335038ce2030
Matan Kushner <matchai@Matans-MacBook-Air.local>
2024-10-28 17:42:45 +0900

    Get things working with fish

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
       nix.settings.experimental-features = "nix-command flakes";
 
       # Create /etc/zshrc that loads the nix-darwin environment.
-      # programs.zsh.enable = true;  # default shell on catalina
+      programs.zsh.enable = true;  # default shell on catalina
       programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
